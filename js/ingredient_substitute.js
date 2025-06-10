2b9592c6b262979b6d7d00fac3368ba40b0dc7e9
Sheryl-Personal <sherylyang79@gmail.com>
2025-06-10 14:02:34 -0400

    began expanding on ingredient substitute results

--- js/ingredient_substitute.js
+++ js/ingredient_substitute.js
@@ -46,14 +46,52 @@ submitButton.addEventListener("click", async (e) => {
         for (i = 0 ; i < json.substitutes.length; i ++){
             substitutes += `<li> ${json.substitutes[i]} </li> `;
         };
+        
+        /* ------------ FETCHING INFORMATION ------------ */
+        //FETCH INGREDIENT ID 
+        
+        //API Query 
+        const id_query = `https://api.spoonacular.com/food/ingredients/search?query=${ingredient}&apiKey=${key}`; 
+        
+        //API Array 
+        const id_response = await fetch(id_query); 
+        const id_json = await id_response.json(); 
 
-        /* Show substitute options */ 
+        //ID & Image successfully fetched 
+        let ingredient_arr = id_json.results[0]; 
+        let ingredient_id = ingredient_arr.id; 
+        let ingredient_img = ingredient_arr.image; 
+
+        //API Query
+        const ingredient_info = `https://api.spoonacular.com/food/ingredients/${ingredient_id}/information?amount=1&apiKey=${key}`;
+
+        // API Array 
+        const info_response = await fetch(ingredient_info); 
+        const info_json = await info_response.json(); 
+        console.log(info_json); 
+
+
+
+       //Show substitute options
         substitute_result.innerHTML = 
             ` 
+            <div>
+                <div class = "row">
+                    <div class = "description col"> 
+                        <h2> ${recipe_name} </h2> 
+                        <h3> Diets: ${recipe_diets} </h3> 
+                        <h3> Time: ${recipe_time} </h3> 
+                        <h3> Servings: ${recipe_servings} </h3> 
+                        <p> ${recipe_description} </p> 
+                    </div>
+                    <div class = "image col">
+                        <img src = ${recipe_photo}>
+                    </div>
+                </div> 
+            </div>
             <h4> ${message} </h4> 
             <ol> ${substitutes} </ol> 
             `; 
     }
-
 });
 /* ------------ END OF SUBMISSION BUTTON ------------ */
